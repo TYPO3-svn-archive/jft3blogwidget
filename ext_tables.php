e6ce13aa7bd0fb1cac9b46df42bb47f67e1993b7
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-21 13:58:42 +0000

    Add backend style for T3BLOG / Add jQuery 1.6.1 / Add jQuery UI 1.8.13
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jft3blogwidget/trunk@47967 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,24 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'T3BLOG jQuery Widgets');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'T3BLOG jQuery Widgets');
 
+if (TYPO3_MODE == 'BE') {
+	$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+
+	if ($confArr['t3blogThemeEnable']) {
+		$GLOBALS['TBE_STYLES']['skins'][$_EXTKEY] = array(
+			"name" => $_EXTKEY,
+			"stylesheetDirectories" => array(
+				"sprites" => $confArr['t3blogTheme']
+			)
+		);
+	}
+	// define the icon, if not
+	if (! isset($ICON_TYPES['t3blog'])) {
+		$ICON_TYPES['t3blog'] = array(
+			'icon' => t3lib_extMgm::extRelPath('t3blog') . 'ext_icon.gif'
+		);
+	}
+}
 ?>
